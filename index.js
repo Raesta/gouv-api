56009327ee7d37041b6348329c223df95b9047b5
Raesta <dvs75019@gmail.com>
2016-06-24 17:33:24 +0200

    Implement NationalAddressAPI & update version

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var version = '0.0.1';
+var NationalAddressAPI = require('gouv-national-address-api');
+var version = '1.0.0';
 
 function Gouv() {
   this.version = version;
+  this.nationalAddress = new NationalAddressAPI();
 }
 
 module.exports = Gouv;
