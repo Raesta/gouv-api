0917181a9f66af586b248cd6bb5d703dc401842f
Raesta <dvs75019@gmail.com>
2016-06-27 00:25:27 +0200

    Add gouv-geo-api & upgrade version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var NationalAddressAPI = require('gouv-national-address-api');
-var version = '1.0.0';
+var GeoAPI = require('gouv-geo-api');
+var version = '1.1.0';
 
 function Gouv() {
   this.version = version;
   this.nationalAddress = new NationalAddressAPI();
+  this.Geo = new GeoAPI();
 }
 
 module.exports = Gouv;
