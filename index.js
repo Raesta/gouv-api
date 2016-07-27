3330f62131d98a919b244478735980106f24d15b
Raesta <dvs75019@gmail.com>
2016-07-27 13:30:25 +0200

    Add gouv-openfisca-api & upgrade version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var NationalAddressAPI = require('gouv-national-address-api');
 var GeoAPI = require('gouv-geo-api');
-var version = '1.1.0';
+var OpenfiscaAPI = require('gouv-openfisca-api');
+var version = '1.2.0';
 
 function Gouv() {
   this.version = version;
   this.nationalAddress = new NationalAddressAPI();
   this.Geo = new GeoAPI();
+  this.Openfisca = new OpenfiscaAPI();
 }
 
 module.exports = Gouv;
